5d49c1b87d35aa52d7d8db64bdedebc9ff8f56d4
Alphin Shajan <alphinshajan2004@gmail.com>
2024-06-09 16:24:43 -0400

    Started the front end of currency converter

---
+++ Redirect.js
@@ -0,0 +1,6 @@
+function redirectToHome() { window.location.href = 'home.html'; }
+
+function redirectToBrowseAllCurrenices() { window.location.href = 'browseAllCurrenices.html'; }
+
+function redirectToLiveRates() { window.location.href = 'liveRates.html'; }
+
