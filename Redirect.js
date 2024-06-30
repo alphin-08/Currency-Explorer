69353da73f67caed8b7f400e019b98eb64b4b779
Alphin Shajan <alphinshajan2004@gmail.com>
2024-06-30 08:00:08 -0400

    Deleted old front end

--- Redirect.js
+++ Redirect.js
@@ -1,7 +1,3 @@
-function redirectToHome() { window.location.href = 'home.html'; }
 
-function redirectToBrowseAllCurrenices() { window.location.href = 'browseAllCurrenices.html'; }
-
-function redirectToLiveRates() { window.location.href = 'liveRates.html'; }
 
 
